5dfbe5da332ee2bddb6a5980f50e6ceff9e49118
sasinha <sinhasaeng@gmail.com>
2016-10-12 21:04:59 -0700

    packed

--- Hill_Cipher/cipher.py
+++ Hill_Cipher/cipher.py
@@ -1,34 +1,35 @@
-from message_matrix_relation import to_matrix, to_message
-from modulo_inverse_matrix import inverse_matrix, random_mod_matrix
+from Hill_Cipher import message_matrix_relation as mmr
 import numpy as np
 
-# characters = 'abcdefghijklmnopqrstuvwxyz /"'
-characters = '013456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\x0b2'
+from Hill_Cipher import modulo_inverse_matrix as mim
+
+characters = '0213456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\x0b2'
 
 
 def encrypt(input_message, dimension):
 
     modulo = len(characters) - 1
     input_message_matrix_dimension = (dimension, np.ceil(len(input_message) / dimension).astype(int))
-    input_message_matrix = to_matrix(input_message, input_message_matrix_dimension, characters)
+    input_message_matrix = mmr.to_matrix(input_message, input_message_matrix_dimension, characters)
     cipher_matrix_dimension = (dimension, dimension)
-    cipher_matrix = random_mod_matrix(0, modulo, cipher_matrix_dimension)
-    key_matrix = inverse_matrix(cipher_matrix, modulo)
+    cipher_matrix = mim.random_mod_matrix(0, modulo, cipher_matrix_dimension)
+    key_matrix = mim.inverse_matrix(cipher_matrix, modulo)
     scrambled_message_matrix = np.mod(np.dot(cipher_matrix, input_message_matrix), modulo)
-    return to_message(scrambled_message_matrix, characters) , to_message(key_matrix, characters)
+    return mmr.to_message(scrambled_message_matrix, characters) , mmr.to_message(key_matrix, characters)
 
 
 def decrypt(input_message, key):
+
     modulo = len(characters) - 1
     key_matrix_column_length = int(len(key) ** .5)
     key_matrix_dimension = (key_matrix_column_length, key_matrix_column_length)
     scrambled_message_matrix_dimension = (
         key_matrix_column_length, int(np.ceil(len(input_message) / key_matrix_column_length))
     )
-    key_matrix = to_matrix(key, key_matrix_dimension, characters)
-    scrambled_message_matrix = to_matrix(input_message, scrambled_message_matrix_dimension, characters)
+    key_matrix = mmr.to_matrix(key, key_matrix_dimension, characters)
+    scrambled_message_matrix = mmr.to_matrix(input_message, scrambled_message_matrix_dimension, characters)
     unscrambled_message_matrix = np.mod(np.dot(key_matrix, scrambled_message_matrix), modulo)
-    return to_message(unscrambled_message_matrix, characters)
+    return mmr.to_message(unscrambled_message_matrix, characters)
 
 
 
@@ -36,7 +37,7 @@ def decrypt(input_message, key):
 # Test
 #
 # message = "hello, how are you? Lets go meet for dinner / "
-# scrambled_message = encrypt(message, 7)
+# scrambled_message = encrypt(message, 3)
 # print(scrambled_message)
 #
 # unscrambled_message = decrypt(scrambled_message[0], scrambled_message[1])
--- Hill_Cipher/message_matrix_relation.py
+++ Hill_Cipher/message_matrix_relation.py
@@ -1,6 +1,7 @@
 import numpy as np
 from collections import OrderedDict
 
+
 def to_matrix(message, dimension, character_string):
     modulo = len(character_string) - 1
     character_to_number = OrderedDict(zip(character_string, range(0, modulo)))
@@ -12,7 +13,8 @@ def to_message(matrix, character_string):
     number_to_character = OrderedDict(zip(range(0, modulo), character_string))
     return ''.join(number_to_character[x] for x in list(np.concatenate(list(matrix))))
 
-#
+
+
 # characters = '013456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\x0b2'
 # test = to_matrix("013456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVW XYZ!", (10, 3), characters)
 # testWork = to_message(test, characters)
