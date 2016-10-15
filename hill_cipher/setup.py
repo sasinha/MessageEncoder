dca45ae8a5c761dbaadeb238b5fdcf3afbb8c494
sasinha <sinhasaeng@gmail.com>
2016-10-14 18:11:28 -0700

    packing, added setup and requirements

---
+++ hill_cipher/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name = 'hill_cipher',
+      version = '1.0',
+      description= 'Simple hill cipher for messages',
+      url='https://github.com/sasinha/MessageEncoder',
+      author='sasinha',
+      author_email='sinhasaeng@gmail.com',
+      license='MIT',
+      packages=find_packages()
+      )
